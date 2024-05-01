564296ff4d2163badd56919c15aea5646474b436
ekdms <ekdms@192.168.219.106>
2024-05-02 01:12:13 +0900

    5/2 호현씨 과제

--- home/src/main/webapp/js/todolist.js
+++ home/src/main/webapp/js/todolist.js
@@ -1,3 +1,70 @@
 //입력하면 add 누르면 밑에 추가되는거
 //아무것도 입력안하고 add누르면 알림창뜨기
 //x누르면 삭제되게
+//밑줄되는거
+
+//console.log(inp);
+let addBtn = document.querySelector('.addBtn');
+//console.log(addBtn);
+
+let li = document.querySelectorAll('#myUL li');
+//console.log(li);
+li.forEach(function(content){ 
+	let span = document.createElement('span');
+	let txt = document.createTextNode("\u00D7");
+	span.className = 'close'; 
+	span.appendChild(txt);
+	
+	span.addEventListener('click',function(){
+		content.remove();
+	})
+	content.appendChild(span);
+})
+
+addBtn.addEventListener('click', function() {
+	let inp = document.querySelector('#myInput').value; //이유
+	//console.log(inp);
+	if(inp == ''){
+		alert('입력하세요');
+	}else{
+		let li = document.createElement('li');
+		let ul = document.querySelector('#myUL');
+		//console.log(ul);
+		li.innerHTML = inp;
+		let span = document.createElement('span');
+		let txt = document.createTextNode("\u00D7");
+		span.className = 'close';
+		span.appendChild(txt);
+		
+		span.addEventListener('click',function(){
+			li.remove();
+		})
+		
+		li.appendChild(span);
+		ul.appendChild(li);
+	}
+	//inp지워(초기화)
+	document.querySelector('#myInput').value = '';
+})
+
+let ul = document.querySelector('#myUL').addEventListener('click', function(el){
+	//console.log(el.target); //=윈도우 객체의 타겟 가져와
+	let target = el.target; //클릭한 li 
+	//console.log(el);
+	if(target.className == 'checked'){ //li의 class만 가져왔으니까 나머지 li도 가져와야하니까
+		target.className = '';
+	}else{
+		target.className = 'checked';
+	}
+})
+
+//document.querySelector('.addBtn').addEventListener('click',function(){
+//	console.log(document.querySelector('.addBtn'));
+//})
+
+
+
+
+
+
+
